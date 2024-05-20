2e0863cb4f02cd05543a88ee04864edc369b4316
SEGITIGaa <ilyasjuandaraya2256@gmail.com>
2024-05-20 17:36:06 +0700

    bnerin form

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const FormInput = ({ Submited }) => {
   return (
-    <form action="" onSubmit={Submited} className="flex items-center gap-6">
+    <form action="" onSubmit={Submited} className="flex items-center gap-4">
       <input
         type="search"
         name=""
         id=""
         placeholder="Ask me anything"
-        className="font-semibold text-neutral-300 w-full px-4 py-2 ring-2 outline-none bg-neutral-900 ring-neutral-950 rounded-lg"
+        className="font-semibold text-neutral-900 bg-neutral-700 w-full p-4 outline-none rounded-xl"
       />
       <button
         type="submit"
-        className="bg-white rounded-lg px-4 py-2 text-lg text-neutral-900 font-semibold"
+        className="bg-white rounded-xl p-4 text-lg text-neutral-900 font-semibold"
       >
        <img width="32" height="32" src="https://img.icons8.com/fluency-systems-filled/96/sent.png" alt="sent" className="-rotate-45"/>
       </button>
